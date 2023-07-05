c0238daba01b4af21acc8f41fbe81b19ea7a518e
Sasha test <not.my.deliverance@gmail.com>
2023-07-05 21:39:04 +0300

    task_final_done_1.3

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,23 +34,16 @@
 // }
 
 function sendRequest (name, phone, address, goods, sum) {
-	let data = {goods: [], order: {}};
-	goods = {title: {}, count: []};
+	let data = {order: {address, sum}, client: {name, phone}, goods: []}
+	
 	let countOfGoods = goods.length;
 
-	for (let i = 0; i <= countOfGoods; i + 1) {
-		data.goods.push(goods[i].title);
+	for (let i = 0; i < countOfGoods; i + 1) {
+		data.goods.push({title:goods[i].title, count:goods[i].count});
 	}
-	data.order.address = {
-		street: 'ул. Ленина',
-		house: '2',
-		entrance: '4',
-		floor: '15',
-		flat: '53',
-	  }
+	data.order.address = "ул. "`${address.street}` + ", дом " + `${address.house}` + ", " + `${address.entrance}` + ", подъезд, " + `${address.floor}` + " этаж, кв. " + `${address.flat}`;
 	data.order.sum = sum;
-	data.client = 'Иван' + '+7(987)65-43-210';
-
+	data.client = `${name}` + " " + `${phone}`;
 	let jsonData = JSON.stringify({data});
 
 	return jsonData;
